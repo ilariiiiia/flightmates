ef91a48e58c0cf134fdd9796a234941ed4a878c5
unknown <griel034@gmail.com>
2023-08-26 10:10:50 +0800

    first commit

--- components/Hero.js
+++ components/Hero.js
@@ -32,7 +32,7 @@ const Hero = () => {
             />
             <button
               type="submit"
-              className="text-white absolute right-2.5 bottom-2.5 bg-amber-700 hover:bg-amber-800 focus:ring-2 focus:outline-none focus:ring-amber-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-amber-200 absolute right-2.5 bottom-2.5 bg-amber-700 hover:bg-amber-800 focus:ring-2 focus:outline-none focus:ring-amber-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Search
             </button>
