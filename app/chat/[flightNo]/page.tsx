6a552e8d27b86fbf798123017007a3b5deb5fe40
ilariiiiia <82510639+ilariiiiia@users.noreply.github.com>
2023-08-27 12:32:28 +0000

    Chats are now created when entering a chat that doesn't exist, or retrieved when they do

--- app/chat/[flightNo]/page.tsx
+++ app/chat/[flightNo]/page.tsx
@@ -1,7 +1,29 @@
-export default function Chat() {
-    return <>
-        <h1>
-            Still in development!
-        </h1>
-    </>
+import prisma from "@/prisma/prisma"
+
+async function getChatData(flightNo: string) {
+    const chat = await prisma.chat.findFirst({
+        where: {
+            flightNo
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    });
+    if(chat) return chat;
+    return await prisma.chat.create({
+        data: {
+            flightNo,
+        }
+    })
+}
+
+export default async function Chat({ params } : { params: { flightNo: string } }) {
+    const chatData = await getChatData(params.flightNo);
+    return (
+        <>
+            <h1>
+                Still in development!
+            </h1>
+        </>
+    )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-        <body>
+        <body className="bg-background">
             <Navbar/>
             {children}
         </body>
