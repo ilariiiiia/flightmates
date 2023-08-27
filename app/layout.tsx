48d3d6f38f8af2c2d6137a15ac937a8efbfcfc3d
ilariiiiia <82510639+ilariiiiia@users.noreply.github.com>
2023-08-27 09:56:20 +0000

    Added more metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,11 @@ import React from "react"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Flightmates',
-  description: 'Chat with your flightmates from upcoming flights!',
+    title: 'Flightmates',
+    description: 'Chat with your flightmates from upcoming flights!',
+    generator: "Next.js",
+    applicationName: "Flightmates",
+    authors: [{name: "Nicola Migone", url: "https://www.ilariamigone.eu"}, {name: "Gabriel Noe Napiere", url: "02portfolio.vercel.app"}]
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
